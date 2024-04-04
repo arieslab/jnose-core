db7fcde10b18ba40f5c6f1fca0855ef77f9230be
Tássio Virgínio <tassiovirginio@gmail.com>
2024-04-04 17:49:19 -0300

    adicionando o hash do método e o caminho do método

--- src/main/java/br/ufba/jnose/dto/TestSmell.java
+++ src/main/java/br/ufba/jnose/dto/TestSmell.java
@@ -1,6 +1,9 @@
 package br.ufba.jnose.dto;
 
 import java.io.Serializable;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
 
 public class TestSmell implements Serializable {
     private static final long serialVersionUID = 1L;
@@ -8,6 +11,16 @@ public class TestSmell implements Serializable {
     private String name;
     private String method;
     private String range;
+    private TestClass testClass;
+    private String code;
+
+    public void setCode(String code){
+        this.code = code;
+    }
+
+    public String getCode(){
+        return code;
+    }
 
     public String getRange() {
         return range;
@@ -31,6 +44,45 @@ public class TestSmell implements Serializable {
 
     public void setMethod(String method) {
         this.method = method;
+    }
+
+    public void setTestClass(TestClass testClass){
+        this.testClass = testClass;
+    }
+
+    public TestClass getTestClass(){
+        return this.testClass;
+    }
+
+    public String getMethodNameHash(){
+        String hash = "";
+        try {
+            MessageDigest md5 = MessageDigest.getInstance("MD5");
+            md5.update(StandardCharsets.UTF_8.encode(this.method));
+            hash = String.format("%032x", new BigInteger(1, md5.digest()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return hash;
+    }
+
+    public String getMethodNameFullURIHash(){
+        if(this.testClass == null){
+            System.out.println("testClass = null ===========================");
+        }
+        String nomeProjeto = this.testClass.getProjectName();
+        String nomeClasse = this.testClass.getFullName();
+        String nomeMetodo = this.method;
+        String baseText = nomeProjeto + nomeClasse + nomeMetodo;
+        String hash = "";
+        try {
+            MessageDigest md5 = MessageDigest.getInstance("MD5");
+            md5.update(StandardCharsets.UTF_8.encode(baseText));
+            hash = String.format("%032x", new BigInteger(1, md5.digest()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return hash;
     }
 
     @Override
